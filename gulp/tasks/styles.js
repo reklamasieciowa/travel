5e23b3c9af6a5206afdd150c4ecac2da260799c4
Michał Brzozowski <biuro@reklamasieciowa.pl>
2018-01-28 16:59:02 +0100

    css and gulp modified

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
+
+gulp.task('styles', function(){
+	//plik żródłowy, plik docelowy temp, gulp.src() jest asynchroniczna
+	
+	return gulp.src('./app/assets/styles/style.css') //plik żródłowy
+		.pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer])) //postcss zamiast sass/less
+		.on('error', function(errorInfo){
+			console.log(errorInfo.toString());
+			this.emit('end'); // jak będzie error, to uznaj, że koniec funkcji, a nie wywal się
+		})
+		.pipe(gulp.dest('./app/temp/styles')); //folder docelowy temp
+});
