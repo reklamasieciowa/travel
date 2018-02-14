a5dd1e8ca6e12b20c85739e971563920b4d6454e
Michał Brzozowski <biuro@reklamasieciowa.pl>
2018-02-14 21:32:03 +0100

    header and menu before JS

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,13 +4,14 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function(){
 	//plik żródłowy, plik docelowy temp, gulp.src() jest asynchroniczna
 	
 	return gulp.src('./app/assets/styles/style.css') //plik żródłowy
-		.pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer])) //postcss zamiast sass/less
+		.pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer])) //postcss zamiast sass/less
 		.on('error', function(errorInfo){
 			console.log(errorInfo.toString());
 			this.emit('end'); // jak będzie error, to uznaj, że koniec funkcji, a nie wywal się
